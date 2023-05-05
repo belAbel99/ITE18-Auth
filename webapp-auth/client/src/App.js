d3c47b96392404b72a9b8a65cce61d3ec53ce4d7
belAbel99 <johnabel6699@gmail.com>
2023-05-05 11:44:09 +0800

    first commit

---
+++ webapp-auth/client/src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import { Container } from "reactstrap";
+import Home from "./components/Home";
+import Registration from "./components/Registration";
+import Logout from "./components/Logout";
+import { ToastContainer } from "react-toastify";
+import ProductView from "./components/ProductView";
+
+function App() {
+  return (
+    <Container>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/product-details/:id" element={<ProductView />} />
+        </Routes>
+        <ToastContainer />
+      </BrowserRouter>
+    </Container>
+  );
+}
+
+export default App;
